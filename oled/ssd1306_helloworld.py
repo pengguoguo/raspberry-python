46f2a9375e834c9c5f3c88ad0ab83e7e89b0e66b
pengguoguo <lzp20@163.com>
2020-01-06 01:43:25 +0800

    modefied spi

--- oled/ssd1306_helloworld.py
+++ oled/ssd1306_helloworld.py
@@ -2,7 +2,7 @@ from luma.core.interface.serial import i2c, spi
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
 
-serial = spi(device=0, port=0,DC=36,RST=35)
+serial = spi(device=0, port=0, gpio_DC=36, gpio_RST=35)
 
 device = ssd1306(serial)
 
