8517d3e11ef8e7ddc7d5e55682cf82f8fb3e86c1
pengguoguo <lzp20@163.com>
2020-01-06 01:15:51 +0800

    add helloworld python file

---
+++ oled/ssd1306_helloworld.py
@@ -0,0 +1,11 @@
+from luma.core.interface.serial import i2c, spi
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+
+serial = spi(device=0, port=0)
+
+device = ssd1306(serial)
+
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    draw.text((30, 40), "Hello World", fill="white")
